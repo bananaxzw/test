e8db3d9663e9da6029002e0fffb6a15bd33d67ed
bananaxzw <bananaxzw@qq.com>
2013-02-19 21:41:13 +0800

    ajax

---
+++ WebApplication1/JsLib/jsonphandler.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="jsonphandler.ashx.cs" Class="WebApplication1.JsLib.jsonphandler" %>
---
+++ WebApplication1/JsLib/jsonphandler.ashx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.JsLib
+{
+    /// <summary>
+    /// jsonphandler 的摘要说明
+    /// </summary>
+    public class jsonphandler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string callback = context.Request["callback"];
+            string response = string.Format("'value1':'{0}','value2':'{1}'", context.Request.QueryString["p1"], context.Request.QueryString["p2"]);
+            string call = callback + "({" + response + "})";
+            context.Response.Write(call);
+
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
